52ad049d0652cc72fd4904a5a8d4210fb1a57abd
m4niff <164046124+m4niff@users.noreply.github.com>
2025-06-20 18:23:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ function createBot() {
   bot.on('spawn', () => {
     console.log('✅ Bot joined');
 
-    // ✅ Force survival if in spectator mode
-    if (bot.game.gameMode !== 'survival') {
-      bot.chat('/gamemode survival');
-    }
-
     // ✅ Random movement to avoid idle
     const directions = ['forward', 'back', 'left', 'right'];
     setInterval(() => {
@@ -58,22 +53,42 @@ function createBot() {
 
     // ✅ Chat loop
     const messages = [
-      "where the fuck am i tf?",
+       "where the fuck am i tf?",
       "pahal aku teperangkap anjj",
       "lepaskan saya lepaskan sayaa saya ketua lanun",
       "oh shi aku lupa aku hanyalah robot hm",
       "bising bdo karina",
-      "amirul fadif x nurul iman",
+      "amirul hadif x nurul iman",
       "gpp jadi sok asik asalkan aq tolong on kan server ni 24 jam",
       "duatiga duatiga dua empat",
       "boikot perempuan nme sofea pantek jubo lahanat",
       "bising do bal",
+      "man ko un sme man stfu aq tk tipu ni wt aq baran gile af like icl ts pmo sm hg ptut senyp sybau like bro ts sgt kevin",
       "SHOW ME YO WILLYYYYY",
       "apa ko aku bukan yatim",
       "blablablbelbelbleblulbu",
+      "ahhhh yes king",
       "sunyi siak",
       "MUSTARRRRRRRDDDDDDDD",
+      "setiap pendosa pasti taubat..dengan itu cukuplah menyebut dosa orang kerana anda juga mempunyai dosa tetapi Allah menutup aibmu.",
+      "because he kno how tu play futbal more than ronaldo",
+      "how gud is that dihh yes king auhghh",
+      "sat berak sat",
+      "tkpe a tk jdi,kentut aja",
+      "asal korg senyap ja",
+      "WOIIIII TK LARAT NI WE",
+      "sedar tk sedar pada satu hari nanti kita tidak jumpa lagi jadi bermainlah selagi ada masa",
+      "sybau",
+      "23duatiga24",
+      "stecu stecu stelan cuek baru malu aduh adik ini mw juga abang yang rayu",
+      "kalau harini menanggg!! gloryglory man unitedddd",
+      "said im fine said i move on",
+      "aku selalu tersenyummmm",
+      "nampak bosan kan? tapi game chat ni bole buat kita pejamkan mata dan bayangkan muka iman",
+      "AUFFUDUDHDUDH SAKIT KEPALA AKU WIWOWUFJWOCBWOCJDOF TOLONG AKUH",
+      "SubhanAllah Alhamdulillah AstagfiruAllah Lailaha ilallah Allahu Akbar",
       "taubat ygy",
+      "kalaulah aku bleh main ngan korg hm",
       "SAYA ULANGGG!!!"
     ];
     let index = 0;
